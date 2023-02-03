d4f3cc5961d959af3a5a5e04ac64fe2cd2d97614
imanol <imanol@easytechgreen.com>
2023-02-03 04:09:00 -0300

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
 import '../styles/globals.scss'
-
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="g-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });;
+        `}
+      </Script>
+      <Component {...pageProps} /></>
   )
 }
