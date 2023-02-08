d51b4efaa605963fe79b705135702108022a13b2
imanol <imanol@easytechgreen.com>
2023-02-08 15:07:16 -0300

    Add a component to prevent rehydration issues

---
+++ components/client-only/client-only.tsx
@@ -0,0 +1,25 @@
+import { FC, HTMLAttributes, ReactNode, useEffect, useState } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+
+const ClientOnly: FC<Props & HTMLAttributes<HTMLDivElement>> = ({ children, ...delegated }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return (
+    <div {...delegated}>
+      {children}
+    </div>
+  );
+};
+
+export default ClientOnly;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,25 +6,28 @@ import HeadComponent from '@components/head/head'
 import Sidebar from '@components/sidebar/sidebar'
 import YouTubeVideo from '@components/youtube-video/youtube-video'
 import { useLocalStorage } from '@hooks/useLocaleStorage'
+import ClientOnly from '@components/client-only/client-only'
 
 export default function Home() {
   const [channelSelected, setChannelSelected] = useLocalStorage('channelSlected', channels[0]);
   return (
     <>
-      <HeadComponent />
-      <main className={styles['main']}>
-        <Sidebar
-          channels={channels}
-          channelSelected={channelSelected}
-          onHandleChannel={setChannelSelected}
-        />
-        <div className={styles['video']}>
-          <YouTubeVideo
-            title={channelSelected.name}
-            videoId={channelSelected.id}
+      <ClientOnly>
+        <HeadComponent />
+        <main className={styles['main']}>
+          <Sidebar
+            channels={channels}
+            channelSelected={channelSelected}
+            onHandleChannel={setChannelSelected}
           />
-        </div>
-      </main>
+          <div className={styles['video']}>
+            <YouTubeVideo
+              title={channelSelected.name}
+              videoId={channelSelected.id}
+            />
+          </div>
+        </main>
+      </ClientOnly>
       <footer></footer>
     </>
   )
